5cf4250e67694382b3ce544c94c96d0ac8e34e94
sneha2999 <athawalesneha8@gmail.com>
2023-12-29 20:49:48 +0530

    module 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import { StyledEngineProvider } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { fetchNewAlbums, fetchSongs, fetchTopAlbums } from "./api/api";
+import Card from './components/Card/Card';
 
 function App() {
+  const [searchData, useSearchData] = useState();
+  const [data, setData] = useState({});
+
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevData) => {
+        return {...prevData, [key]: data};
+      });
+    })
+  }
+
+  useEffect(() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+    generateData("songs", fetchSongs);
+
+  }, [])
+  const {topAlbums = [], newAlbums = [], songs = []} = data;
   return (
     <>
-      <Navbar/>
-      <Hero/>
+      <StyledEngineProvider injectFirst>
+        <Navbar searchData={[...topAlbums, ...newAlbums]} />
+        <Outlet context={{ data: {topAlbums, newAlbums, songs} }}/>
+      </StyledEngineProvider>
     </>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,13 +3,14 @@ import styles from './Navbar.module.css';
 import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
         <nav className={styles.navbar}>
-            <a href="/">
+            <Link to="/">
                 <Logo/>
-            </a>
+            </Link>
             <Search placeholder="Search a song of your choice" />
             <Button>Give Feedback</Button>
         </nav>
