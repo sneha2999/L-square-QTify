e4b76944a677aae26c0d08035107764d59446f46
sneha2999 <athawalesneha8@gmail.com>
2023-12-29 22:44:21 +0530

    module 5

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import { useOutletContext } from "react-router-dom";
 import Hero from "../../components/Hero/Hero";
 import Section from "../../components/Section/Section";
 import styles from "./HomePage.module.css";
+import { fetchFilters } from "../../api/api";
 
 function HomePage() {
    const { data } = useOutletContext();
@@ -13,6 +14,12 @@ function HomePage() {
         <div className={styles.wrapper}>
             <Section title="Top Albums" data={topAlbums} type="album" />
             <Section title="New Albums" data={newAlbums} type="album" />
+            <Section
+                title="Songs"
+                data={songs} 
+                filterSource={fetchFilters}
+                type="song"
+            />
         </div>
     </>
    )
